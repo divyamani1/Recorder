0147856a8696c933499b9659daea6eb203677c8c
divyamani <dma.htd@gmail.com>
2018-08-13 22:33:02 +0545

    Fix DataBase config

--- students/config.py
+++ students/config.py
@@ -1,4 +1,5 @@
 import os
+from sqlalchemy import create_engine
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -6,6 +7,9 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '9=iensbkbpl#*%s)q(0ts=)o4co%(3_l_&85v+px=asu$icm$j'
 
     # The DB credential has been hardcoded only for Project demo. Never write credentials to file.
+    # Replace user and password with your mysql password respedtively.
+    # This should work with other SQL database too. Try by changing mysql:// to your DB.
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://user:password@localhost/students'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://user:password@localhost/'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    engine = create_engine(SQLALCHEMY_DATABASE_URI)
